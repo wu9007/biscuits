a168d634dd78f202f58a3e9ad4132f9097550ef1
wujianchuan <wujianchuan@shinow.com>
2019-01-21 17:16:47 +0800

    删除多余代码，调整包结构。

--- src/main/java/org/hunter/skeleton/Application.java
+++ src/main/java/org/hunter/skeleton/Application.java
@@ -1,4 +1,4 @@
-package org.homo;
+package org.hunter.skeleton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author wujianchuan 2018/12/25
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.homo.*","org.hunter.pocket.*"})
+@ComponentScan(basePackages = {"org.hunter.*"})
 @EnableCaching(proxyTargetClass = true)
 public class Application {
     public static void main(final String[] args) {
