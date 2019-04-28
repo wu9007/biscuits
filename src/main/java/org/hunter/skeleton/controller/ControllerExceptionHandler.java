d99eaba0ca639ec42ec4360505d7561301810eac
leyan95 <18231926271@163.com>
2019-04-28 16:21:08 +0800

    update

--- src/main/java/org/hunter/skeleton/controller/ControllerExceptionHandler.java
+++ src/main/java/org/hunter/skeleton/controller/ControllerExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-
 /**
  * @author wujianchuan
  */
@@ -19,11 +17,12 @@ public class ControllerExceptionHandler {
     private static final String FOREIGN_KEY = "FK_";
 
     @ResponseBody
-    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
-    public Body handler(SQLIntegrityConstraintViolationException e) {
-        logger.error("捕获到 SQLIntegrityConstraintViolationException 异常", e.getMessage());
+    @ExceptionHandler(value = Exception.class)
+    public Body handler(Exception e) {
+        e.printStackTrace();
         String message;
         if (e.getMessage().contains(UNIQUE_KEY)) {
+            // SQLIntegrityConstraintViolationException
             message = "系统中存在与当前记录相同的数据，不允许重复提交。";
         } else if (e.getMessage().contains(FOREIGN_KEY)) {
             message = "您当前操作的数据已被其他业务应用，无法执行删除操作，请先撤销后续业务后再执行删除。";
@@ -31,12 +30,5 @@ public class ControllerExceptionHandler {
             message = e.getMessage();
         }
         return Body.newWaringInstance("失败", message, e.getStackTrace());
-    }
-
-    @ResponseBody
-    @ExceptionHandler(value = Exception.class)
-    public Body handler(Exception e) {
-        e.printStackTrace();
-        return Body.newWaringInstance("失败", e.getMessage(), e.getStackTrace());
     }
 }
