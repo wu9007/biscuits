09a14d451f7be12d22f0d58f928cbdfeca08c965
leyan95 <18231926271@163.com>
2019-05-06 16:13:01 +0800

    update

---
+++ src/main/java/org/hunter/skeleton/spine/model/Pair.java
@@ -0,0 +1,39 @@
+package org.hunter.skeleton.spine.model;
+
+import org.hunter.pocket.annotation.Column;
+import org.hunter.pocket.annotation.Entity;
+
+import java.io.Serializable;
+
+@Entity(table = "", tableId = -1)
+public class Pair implements Serializable {
+    private static final long serialVersionUID = 5656710925928861884L;
+    @Column(name = "lable")
+    private String label;
+    @Column(name = "value")
+    private String value;
+
+    public Pair() {
+    }
+
+    public Pair(String label, String value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
