78c7efa3d66901c1eda20ed1382e9c8fb441daae
leyan95 <18231926271@163.com>
2019-05-08 19:32:23 +0800

    Enhance LocalDateTimeDeserializer.

--- src/main/java/org/hunter/skeleton/jsonserializer/LocalDateTimeDeserializer.java
+++ src/main/java/org/hunter/skeleton/jsonserializer/LocalDateTimeDeserializer.java
@@ -9,13 +9,21 @@ import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author wujianchuan
  */
 public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+    private static final String ZULU = "Z";
+
     @Override
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        return LocalDateTime.ofInstant(Instant.parse(jsonParser.getText()), ZoneId.systemDefault());
+        String text = jsonParser.getText();
+        if (text.endsWith(ZULU)) {
+            return LocalDateTime.ofInstant(Instant.parse(text), ZoneId.systemDefault());
+        } else {
+            return LocalDateTime.parse(text, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
     }
 }
