3840e8506e0a7327d173d392f9cebc65a991c245
leyan95 <18231926271@163.com>
2019-09-15 14:30:41 +0800

    update.

--- src/main/java/org/hunter/skeleton/launcher/MediatorLauncher.java
+++ src/main/java/org/hunter/skeleton/launcher/MediatorLauncher.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -21,16 +22,18 @@ public class MediatorLauncher implements CommandLineRunner {
     private final ApplicationContext context;
 
     @Autowired
-    public MediatorLauncher(List<Mediator> mediators, ApplicationContext context) {
+    public MediatorLauncher(@Nullable List<Mediator> mediators, ApplicationContext context) {
         this.mediators = mediators;
         this.context = context;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        for (Mediator mediator : mediators) {
-            for (Class<? extends Service> serviceClazz : mediator.getServiceClazzList()) {
-                mediator.registerService(context.getBean(serviceClazz));
+        if (this.mediators != null) {
+            for (Mediator mediator : mediators) {
+                for (Class<? extends Service> serviceClazz : mediator.getServiceClazzList()) {
+                    mediator.registerService(context.getBean(serviceClazz));
+                }
             }
         }
     }
