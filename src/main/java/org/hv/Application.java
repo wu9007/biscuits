e360beef7dbae9126ba902e7c7bcecb0069e1391
leyan95 <18231926271@163.com>
2020-05-06 15:53:08 +0800

    修改启动类。

--- src/main/java/org/hv/Application.java
+++ src/main/java/org/hv/Application.java
@@ -1,11 +1,7 @@
 package org.hv;
 
-import org.hv.biscuits.core.BiscuitsConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -16,22 +12,5 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    CommandLineRunner getCommandLineRunner() {
-        return new CommandLineRunner() {
-            @Autowired
-            private BiscuitsConfig biscuitsConfig;
-
-            @Override
-            public void run(String... args) throws Exception {
-                this.biscuitsConfig.init(true)
-                        .runWithDevelopEnvironment()
-                        .setBiscuitsDatabaseSessionId("biscuits")
-                        .resetPersistencePermission("test1", this.biscuitsConfig.getPermissionMap())
-                        .persistenceMapper("test1", this.biscuitsConfig.getActionMap());
-            }
-        };
     }
 }
---
+++ src/main/java/org/hv/biscuits/Launcher.java
@@ -0,0 +1,32 @@
+package org.hv.biscuits;
+
+import org.hv.biscuits.core.BiscuitsConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wujianchuan
+ */
+@Component
+@Order
+public class Launcher implements CommandLineRunner {
+
+    private final BiscuitsConfig biscuitsConfig;
+    @Value("${spring.application.name}")
+    private String serverId;
+
+    public Launcher(BiscuitsConfig biscuitsConfig) {
+        this.biscuitsConfig = biscuitsConfig;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.biscuitsConfig.init(true)
+                .runWithDevelopEnvironment()
+                .setBiscuitsDatabaseSessionId("biscuits")
+                .resetPersistencePermission(serverId, this.biscuitsConfig.getPermissionMap())
+                .persistenceMapper(serverId, this.biscuitsConfig.getActionMap());
+    }
+}
