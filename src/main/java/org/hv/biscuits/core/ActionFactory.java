394bcb0dd75d1fbf33dd23f2e8fbd0c8c565dd4a
leyan95 <18231926271@163.com>
2020-05-06 17:28:34 +0800

    修改启动类。

--- src/main/java/org/hv/biscuits/core/ActionFactory.java
+++ src/main/java/org/hv/biscuits/core/ActionFactory.java
@@ -7,9 +7,11 @@ import org.hv.biscuits.core.views.BundleView;
 import org.hv.biscuits.core.views.PermissionView;
 import org.hv.biscuits.permission.Permission;
 import org.springframework.context.ApplicationContext;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -24,11 +26,13 @@ public class ActionFactory {
     private final static Map<String, PermissionView> PERMISSION_MAP = new ConcurrentHashMap<>();
 
     private final List<Object> bundleList;
-    private final List<Permission> permissions;
+    private List<Permission> permissions = new ArrayList<>();
 
-    ActionFactory(ApplicationContext applicationContext, List<Permission> permissions) {
+    ActionFactory(ApplicationContext applicationContext, @Nullable List<Permission> permissions) {
         bundleList = Arrays.asList(applicationContext.getBeansWithAnnotation(Controller.class).values().toArray());
-        this.permissions = permissions;
+        if (permissions != null) {
+            this.permissions = permissions;
+        }
     }
 
     protected void init() {
