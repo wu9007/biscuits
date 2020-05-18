c501fee0108a6cc0e79fc694620899bc8d40c588
leyan95 <18231926271@163.com>
2020-05-18 09:14:50 +0800

    添加注释信息。

--- src/main/java/org/hv/biscuits/log/LogQueue.java
+++ src/main/java/org/hv/biscuits/log/LogQueue.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.IntStream;
 
 /**
- * 日志队列
+ * 日志队列（包含：控制器日志，消费者日志，业务执行日志，持久化执行日志）
  *
  * @author wujianchuan
  */
@@ -18,20 +18,39 @@ public class LogQueue {
      */
     private final Queue<AccessorLogView> ACCESSOR_LOG_QUEUE = new ConcurrentLinkedQueue<>();
 
+    /**
+     * 提供控制器日志
+     *
+     * @param accessorLogView {@link AccessorLogView}
+     */
     public void offerAccessorLog(AccessorLogView accessorLogView) {
         ACCESSOR_LOG_QUEUE.offer(accessorLogView);
     }
 
+    /**
+     * 控制器日志批量出队，
+     * 如果批量出队的日志数量大于队列数量则全部出队，
+     * 否则出队指定数量的日志。
+     *
+     * @param size 日志数量
+     * @return 日志实例集合
+     */
     public synchronized Queue<AccessorLogView> pollAccessorLog(int size) {
         Queue<AccessorLogView> queue = new ConcurrentLinkedQueue<>();
         int queueSize = ACCESSOR_LOG_QUEUE.size();
         if (queueSize < size) {
             size = queueSize;
         }
-        IntStream.range(0, size).forEach(accessorLogView -> queue.add(ACCESSOR_LOG_QUEUE.poll()));
+        IntStream.range(0, size).forEach(accessorLogView -> queue.add(this.pollAccessorLog()));
         return queue;
     }
 
+    /**
+     * 控制器日志头节点出队，
+     * 如果队列是空队列则返回 {@code null}
+     *
+     * @return 日志实例
+     */
     public AccessorLogView pollAccessorLog() {
         return ACCESSOR_LOG_QUEUE.poll();
     }
