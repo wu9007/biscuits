1bc4fdaa3b069016edb9e9c6958fcca116a633aa
leyan95 <18231926271@163.com>
2020-06-26 17:31:08 +0800

    update.

--- src/main/java/org/hv/biscuits/controller/exception_headler/SqlIntegrityConstraintExceptionHandler.java
+++ src/main/java/org/hv/biscuits/controller/exception_headler/SqlIntegrityConstraintExceptionHandler.java
@@ -16,6 +16,7 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class SqlIntegrityConstraintExceptionHandler {
 
     private static final String UNIQUE_KEY = "UK_";
+    private static final String DUPLICATE_KEY = "Duplicate";
     private static final String FOREIGN_KEY = "FK_";
 
     @ResponseBody
@@ -24,16 +25,16 @@ public class SqlIntegrityConstraintExceptionHandler {
     public Body handler(SQLIntegrityConstraintViolationException e) {
         e.printStackTrace();
         if (e.getMessage() == null) {
-            return Body.warning().title("失败").message(e.toString());
+            return Body.error().title("失败").message(e.toString());
         }
         String message;
-        if (e.getMessage().contains(UNIQUE_KEY)) {
+        if (e.getMessage().contains(UNIQUE_KEY) || e.getMessage().contains(DUPLICATE_KEY)) {
             message = "系统中存在与当前记录相同的数据，不允许重复提交。";
         } else if (e.getMessage().contains(FOREIGN_KEY)) {
             message = "您当前操作的数据已被其他业务应用，无法执行删除操作，请先撤销后续业务后再执行删除。";
         } else {
             message = e.getMessage();
         }
-        return Body.warning().title("失败").message(message);
+        return Body.error().title("失败").message(message);
     }
 }
